1db403663824ee1a3d331fd1477e863f36d06a76
tejaswini972 <52440474+tejaswini972@users.noreply.github.com>
2019-07-30 11:09:57 +0530

    added lastline

--- app/config.js
+++ app/config.js
@@ -24,3 +24,14 @@ module.exports = {
     }
 
 };
+module.exports = {
+
+    bitnami:
+    {
+        name: "MongoDB Service",
+        url: "mongodb://" + process.env.DATABASE_USERNAME + ":" + process.env.DATABASE_PASSWORD + "@" + process.env.DATABASE_HOST + "/" + process.env.DATABASE_NAME,
+        port: process.env.DATABASE_PORT
+    }
+
+};
+
